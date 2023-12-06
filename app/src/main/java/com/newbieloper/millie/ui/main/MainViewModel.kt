f873f24400ec56509ea025d9005aa3ee30f980ec
Newbieloper <newbieloper@gmail.com>
2023-12-07 04:25:14 +0900

    Main // 가로 사이즈 600dp 레이아웃 추가 | 뉴스 아이템 테두리 제거 | 뉴스 데이터 호출 로직 추가

--- app/src/main/java/com/newbieloper/millie/ui/main/MainViewModel.kt
+++ app/src/main/java/com/newbieloper/millie/ui/main/MainViewModel.kt
@@ -2,18 +2,23 @@ package com.newbieloper.millie.ui.main
 
 import androidx.lifecycle.viewModelScope
 import com.newbieloper.millie.core.base.LoadingViewModel
+import com.newbieloper.millie.data.Result
+import com.newbieloper.millie.data.remote.article.ArticleRepository
+import com.newbieloper.millie.data.successOr
 import com.newbieloper.millie.ui.main.model.Article
-import com.newbieloper.millie.ui.main.model.Source
+import com.newbieloper.millie.ui.main.model.toArticle
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
-import java.time.LocalDateTime
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
-class MainViewModel @Inject constructor() : LoadingViewModel() {
+class MainViewModel @Inject constructor(
+    private val articleRepository: ArticleRepository
+) : LoadingViewModel() {
 
     private val _showSplashScreen = MutableStateFlow(true)
     val showSplashScreen = _showSplashScreen.asStateFlow()
@@ -22,92 +27,39 @@ class MainViewModel @Inject constructor() : LoadingViewModel() {
     val articleList = _articleList.asStateFlow()
 
     init {
+        getTopHeadlineArticleList()
+    }
+
+    private fun hideSplashScreen() {
         viewModelScope.launch {
-            _articleList.emit(mutableListOf<Article>().apply {
-                add(
-                    Article(
-                        title = "title",
-                        author = "author",
-                        content = "content",
-                        description = "description",
-                        url = "url",
-                        urlToImage = "urlToImage",
-                        publishedAt = LocalDateTime.now(),
-                        source = Source(
-                            id = "id",
-                            name = "name"
-                        )
-                    )
-                )
-                add(
-                    Article(
-                        title = "title",
-                        author = "author",
-                        content = "content",
-                        description = "description",
-                        url = "url",
-                        urlToImage = "urlToImage",
-                        publishedAt = LocalDateTime.now(),
-                        source = Source(
-                            id = "id",
-                            name = "name"
-                        )
-                    )
-                )
-                add(
-                    Article(
-                        title = "title",
-                        author = "author",
-                        content = "content",
-                        description = "description",
-                        url = "url",
-                        urlToImage = "urlToImage",
-                        publishedAt = LocalDateTime.now(),
-                        source = Source(
-                            id = "id",
-                            name = "name"
-                        )
-                    )
-                )
-                add(
-                    Article(
-                        title = "title",
-                        author = "author",
-                        content = "content",
-                        description = "description",
-                        url = "url",
-                        urlToImage = "urlToImage",
-                        publishedAt = LocalDateTime.now(),
-                        source = Source(
-                            id = "id",
-                            name = "name"
-                        )
-                    )
-                )
-                add(
-                    Article(
-                        title = "title",
-                        author = "author",
-                        content = "content",
-                        description = "description",
-                        url = "url",
-                        urlToImage = "urlToImage",
-                        publishedAt = LocalDateTime.now(),
-                        source = Source(
-                            id = "id",
-                            name = "name"
-                        )
-                    )
-                )
-            })
-            delay(1000)
-            hideSplashScreen()
+            _showSplashScreen.emit(false)
         }
     }
 
-    fun hideSplashScreen() {
+    private fun getTopHeadlineArticleList() {
         viewModelScope.launch {
-            _showSplashScreen.emit(false)
+            articleRepository.getTopHeadlineArticleList().collectLatest { result ->
+                when (result) {
+                    is Result.Success -> {
+                        val response = result.successOr(null)
+                        if (response != null) {
+                            _articleList.emit(response.articleList.map { it.toArticle() })
+                        }
+                    }
+
+                    is Result.ErrorResponse -> {
+                        // 에러 처리
+                        Timber.e(result.errorBody.toString())
+                    }
+
+                    else -> {
+                        // 에러 처리
+                        Timber.e(result.toString())
+                    }
+                }
+
+                hideSplashScreen()
+            }
         }
     }
 }
