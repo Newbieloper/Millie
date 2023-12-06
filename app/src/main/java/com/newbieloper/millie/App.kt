6ee17205dca96f60b8650f4c9f60eaeb8307ad47
Newbieloper <newbieloper@gmail.com>
2023-12-06 19:48:40 +0900

    App // Hilt 및 Timber 초기화 설정

---
+++ app/src/main/java/com/newbieloper/millie/App.kt
@@ -0,0 +1,12 @@
+package com.newbieloper.millie
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
